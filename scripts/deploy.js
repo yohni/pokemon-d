9c06193245579a76422d2b35f8a2f6019e132353
yohni <yohni.123@gmail.com>
2021-12-17 22:39:23 +0700

    refactor: 💡 deploy attack the boss

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,38 +3,44 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(
     ['Chimchar', 'Turtwig', 'Piplup'],
     [
-      'https://firebasestorage.googleapis.com/v0/b/cataluv-94a78.appspot.com/o/pokemon%2Fstarter_chimchar.png?alt=media',
-      'https://firebasestorage.googleapis.com/v0/b/cataluv-94a78.appspot.com/o/pokemon%2Fstarter_turtwig.png?alt=media',
-      'https://firebasestorage.googleapis.com/v0/b/cataluv-94a78.appspot.com/o/pokemon%2Fstarter_piplup.png?alt=media',
+      'https://firebasestorage.googleapis.com/v0/b/cataluv-94a78.appspot.com/o/pokemon%2Fchimchar.png?alt=media',
+      'https://firebasestorage.googleapis.com/v0/b/cataluv-94a78.appspot.com/o/pokemon%2Fturtwig.png?alt=media',
+      'https://firebasestorage.googleapis.com/v0/b/cataluv-94a78.appspot.com/o/pokemon%piplup.png?alt=media',
     ],
-    [100, 200, 300],
-    [100, 50, 25],
-    ['Fireball', 'Rock Slide', 'Bubble']
+    [200, 250, 300],
+    [50, 40, 30],
+    ['Flame Wheel', 'Razor Leaf', 'Bubble'],
+    'Arceus',
+    'https://firebasestorage.googleapis.com/v0/b/cataluv-94a78.appspot.com/o/pokemon%2Farceus.png?alt=media',
+    10000,
+    90,
+    'Hyper Beam'
   );
   await gameContract.deployed();
   console.log('Contract deployed to:', gameContract.address);
-
   let txn;
+  // We only have three characters.
+  // an NFT w/ the character at index 2 of our array.
   txn = await gameContract.mintCharacterNFT(0);
   await txn.wait();
-  console.log('Minted NFT #1');
 
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log('Minted NFT #2');
-
-  txn = await gameContract.mintCharacterNFT(2);
-  await txn.wait();
-  console.log('Minted NFT #3');
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log('Minted NFT #4');
-
-  console.log('Done deploying and minting!');
   // Get the value of the NFT's URI.
   // let returnedTokenUri = await gameContract.tokenURI(1);
-  // console.log('Token URI:', returnedTokenUri);
+  // console.log('Token URI:', returnedTokenUri);\
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  onsole.log('Done!');
 };
 
 const runMain = async () => {
